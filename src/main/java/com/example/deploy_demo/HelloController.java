5283d4b2b704223c5ecceb8938c978485c80553d
sh-siddharth <siddharth.hota93@gmail.com>
2026-06-21 11:59:19 +0530

    testing

--- src/main/java/com/example/deploy_demo/HelloController.java
+++ src/main/java/com/example/deploy_demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World! My DevOps pipeline works!";
+        return "Hello World! My DevOps pipeline works! Testing Phase";
     }
 }
