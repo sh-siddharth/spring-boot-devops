dc77710da435c60f46bc288c3616a6f81da0bb9b
sh-siddharth <siddharth.hota93@gmail.com>
2026-06-21 15:01:36 +0530

    update home page message

--- src/main/java/com/example/deploy_demo/HelloController.java
+++ src/main/java/com/example/deploy_demo/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello World! My DevOps pipeline works! Testing Phase";
+        return "Wow! My automated pipeline updated this live!";
     }
 }
